bfd3ecdad072d0fb8c2d1c6f3e1d07366e32129b
Asaqa988 <asaqa001@gmail.com>
2024-10-23 21:35:52 +0300

    update till test number 4

--- src/myTestcases.java
+++ src/myTestcases.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,6 +14,8 @@ public class myTestcases {
 	WebDriver driver = new ChromeDriver();
 
 	String WebSiteURL = "https://global.almosafer.com/en";
+	Random rand = new Random(); 
+
 
 	@BeforeTest
 	public void mySetup() {
@@ -27,7 +30,7 @@ public class myTestcases {
 
 	}
 
-	@Test(priority = 1,enabled = false)
+	@Test(priority = 1, enabled = false)
 
 	public void CheckTheEnglishLanguageIsDefault() {
 		String ActualLaguage = driver.findElement(By.tagName("html")).getAttribute("lang");
@@ -36,7 +39,7 @@ public class myTestcases {
 		Assert.assertEquals(ActualLaguage, ExpectedLanguage);
 	}
 
-	@Test(priority = 2,enabled = false)
+	@Test(priority = 2, enabled = false)
 	public void CheckTheDefaultCurrencyIsSAR() {
 		String ActualCurrency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"))
 				.getText();
@@ -46,7 +49,7 @@ public class myTestcases {
 
 	}
 
-	@Test(priority = 3,enabled = false)
+	@Test(priority = 3, enabled = false)
 	public void CheckContactNumber() {
 		String ActualNumber = driver.findElement(By.cssSelector(".sc-hUfwpO.bWcsTG")).getText();
 
@@ -56,64 +59,92 @@ public class myTestcases {
 
 	}
 
-	@Test(priority = 4,enabled = false)
+	@Test(priority = 4, enabled = false)
 	public void CheckQitafLogoIsThereInTheFooter() {
-		
-	WebElement TheFooter = 	driver.findElement(By.tagName("footer")); 
-		boolean ActualResult = TheFooter.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ciodno.lkfeIG")).isDisplayed();
+
+		WebElement TheFooter = driver.findElement(By.tagName("footer"));
+		boolean ActualResult = TheFooter.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ciodno.lkfeIG"))
+				.isDisplayed();
 		boolean ExpectedResult = true;
 
 		Assert.assertEquals(ActualResult, ExpectedResult);
 
 	}
-	
-	@Test(priority = 5,enabled = false)
-	
+
+	@Test(priority = 5, enabled = false)
+
 	public void CheckHotelTabIsNotSelected() {
-		WebElement HotelTab =driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
-		String ExpectedValue = "false" ; 
-		
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ExpectedValue = "false";
+
 		String ActualValue = HotelTab.getAttribute("aria-selected");
-		
+
 		Assert.assertEquals(ActualValue, ExpectedValue);
-		
+
 	}
-	
-	@Test(priority = 6)
+
+	@Test(priority = 6,enabled = false)
 	public void CheckDepatureDate() {
-		int today = LocalDate.now().getDayOfMonth(); 
-		
+		int today = LocalDate.now().getDayOfMonth();
+
 		int Tomorrow = LocalDate.now().plusDays(1).getDayOfMonth();
-		
-		
+
 //		System.out.println(today);
 //		System.out.println(Tomorrow);
 //		System.out.println(DayAfterTomorrow);
 
-		String ActualDepature = driver.findElement(By.cssSelector("div[class='sc-OxbzP sc-lnrBVv gKbptE'] span[class='sc-fvLVrH hNjEjT']")).getText();
+		String ActualDepature = driver
+				.findElement(By.cssSelector("div[class='sc-OxbzP sc-lnrBVv gKbptE'] span[class='sc-fvLVrH hNjEjT']"))
+				.getText();
 		String ExpectedDepature = Integer.toString(Tomorrow);
-		
+
 		Assert.assertEquals(ActualDepature, ExpectedDepature);
 
-;
+		;
 
-System.out.println();
-		
-		
-		                        
+		System.out.println();
+
+	}
+
+	@Test(priority = 7,enabled = false)
+	public void CheckReturnDate() {
+		int today = LocalDate.now().getDayOfMonth();
+
+		int DayAfterTomorrow = LocalDate.now().plusDays(2).getDayOfMonth();
+
+		String ActualReturn = driver
+				.findElement(By.cssSelector("div[class='sc-OxbzP sc-bYnzgO bojUIa'] span[class='sc-fvLVrH hNjEjT']"))
+				.getText();
+		String ExpectedReturn = Integer.toString(DayAfterTomorrow);
+
+		Assert.assertEquals(ActualReturn, ExpectedReturn);
+
 	}
 	
-	@Test(priority = 7)
-	public void CheckReturnDate() {
-		int today = LocalDate.now().getDayOfMonth(); 
+	@Test(priority = 8)
+	public void RandomlyChangeTheLanguage() throws InterruptedException {
+		String [] myWebsites = {"https://www.almosafer.com/ar","https://www.almosafer.com/en"};
 		
-		int DayAfterTomorrow = LocalDate.now().plusDays(2).getDayOfMonth();
+		int randomIndex = rand.nextInt(myWebsites.length);
 		
-		String ActualReturn = driver.findElement(By.cssSelector("div[class='sc-OxbzP sc-bYnzgO bojUIa'] span[class='sc-fvLVrH hNjEjT']")).getText();
-		String ExpectedReturn = Integer.toString(DayAfterTomorrow);
+		driver.get(myWebsites[randomIndex]);
 		
-		Assert.assertEquals(ActualReturn, ExpectedReturn);
+		if(driver.getCurrentUrl().equals("https://www.almosafer.com/ar")) {
+			String ActualLaguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+			String ExpectedLanguage = "ar";
+
+			Assert.assertEquals(ActualLaguage, ExpectedLanguage);
+		}else {
+			String ActualLaguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+			String ExpectedLanguage = "en";
+
+			Assert.assertEquals(ActualLaguage, ExpectedLanguage);
+		}
 		
+	
+		
+	
+
 	}
 
 }

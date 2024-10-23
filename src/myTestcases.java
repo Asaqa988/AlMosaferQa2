3a9f9542b48121213de6bd1075e2a1c721423ef2
Asaqa988 <asaqa001@gmail.com>
2024-10-23 20:59:18 +0300

    till test number 7 soso

--- src/myTestcases.java
+++ src/myTestcases.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -25,7 +27,7 @@ public class myTestcases {
 
 	}
 
-	@Test(priority = 1)
+	@Test(priority = 1,enabled = false)
 
 	public void CheckTheEnglishLanguageIsDefault() {
 		String ActualLaguage = driver.findElement(By.tagName("html")).getAttribute("lang");
@@ -34,7 +36,7 @@ public class myTestcases {
 		Assert.assertEquals(ActualLaguage, ExpectedLanguage);
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2,enabled = false)
 	public void CheckTheDefaultCurrencyIsSAR() {
 		String ActualCurrency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"))
 				.getText();
@@ -44,7 +46,7 @@ public class myTestcases {
 
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 3,enabled = false)
 	public void CheckContactNumber() {
 		String ActualNumber = driver.findElement(By.cssSelector(".sc-hUfwpO.bWcsTG")).getText();
 
@@ -54,13 +56,64 @@ public class myTestcases {
 
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 4,enabled = false)
 	public void CheckQitafLogoIsThereInTheFooter() {
-		boolean ActualResult = driver.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ciodno.lkfeIG")).isDisplayed();
+		
+	WebElement TheFooter = 	driver.findElement(By.tagName("footer")); 
+		boolean ActualResult = TheFooter.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ciodno.lkfeIG")).isDisplayed();
 		boolean ExpectedResult = true;
 
 		Assert.assertEquals(ActualResult, ExpectedResult);
 
+	}
+	
+	@Test(priority = 5,enabled = false)
+	
+	public void CheckHotelTabIsNotSelected() {
+		WebElement HotelTab =driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ExpectedValue = "false" ; 
+		
+		String ActualValue = HotelTab.getAttribute("aria-selected");
+		
+		Assert.assertEquals(ActualValue, ExpectedValue);
+		
+	}
+	
+	@Test(priority = 6)
+	public void CheckDepatureDate() {
+		int today = LocalDate.now().getDayOfMonth(); 
+		
+		int Tomorrow = LocalDate.now().plusDays(1).getDayOfMonth();
+		
+		
+//		System.out.println(today);
+//		System.out.println(Tomorrow);
+//		System.out.println(DayAfterTomorrow);
+
+		String ActualDepature = driver.findElement(By.cssSelector("div[class='sc-OxbzP sc-lnrBVv gKbptE'] span[class='sc-fvLVrH hNjEjT']")).getText();
+		String ExpectedDepature = Integer.toString(Tomorrow);
+		
+		Assert.assertEquals(ActualDepature, ExpectedDepature);
+
+;
+
+System.out.println();
+		
+		
+		
+	}
+	
+	@Test(priority = 7)
+	public void CheckReturnDate() {
+		int today = LocalDate.now().getDayOfMonth(); 
+		
+		int DayAfterTomorrow = LocalDate.now().plusDays(2).getDayOfMonth();
+		
+		String ActualReturn = driver.findElement(By.cssSelector("div[class='sc-OxbzP sc-bYnzgO bojUIa'] span[class='sc-fvLVrH hNjEjT']")).getText();
+		String ExpectedReturn = Integer.toString(DayAfterTomorrow);
+		
+		Assert.assertEquals(ActualReturn, ExpectedReturn);
+		
 	}
 
 }
